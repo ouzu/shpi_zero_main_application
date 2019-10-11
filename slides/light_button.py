385d147f58806ecc4ad51a66b44ffb8e2d4fbe39
ouzu <ouzu@laze.today>
2019-10-11 21:22:26 +0200

    fix ip addresses

--- slides/light_button.py
+++ slides/light_button.py
@@ -42,7 +42,7 @@ def inloop(textchange = False,activity = False, offset = 0):
     peripherals.touch_pressed = False     
   
   if peripherals.clicked(httpbutton.x,httpbutton.y):
-    url = 'http://10.0.1.172:8123/api/services/light/toggle'
+    url = 'http://10.0.3.172:8123/api/services/light/toggle'
     headers = {
       'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI4MGQ0NjA4YjlkMWY0MTkwOGIwNGUxMDZiMzY4MmQxMiIsImlhdCI6MTU3MDgxNzYxNiwiZXhwIjoxODg2MTc3NjE2fQ.rWoJeWwFkQgX726L_-wIYMt3bXXnay8HPMH1UTJec28',
       'content-type': 'application/json',
